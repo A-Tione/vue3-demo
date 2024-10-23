4ef401eff85381d1a6b028d35b1881ed061af87f
Yang.he <yang.he@homepartners.tech>
2024-10-23 15:50:42 +0800

    feat: modify tabs component

---
+++ src/components/Tabs1.demo.vue
@@ -0,0 +1,15 @@
+<demo>常规用法</demo>
+<template>
+  <Tabs v-model:selected="xxx">
+    <Tab title="导航 1">内容 1</Tab>
+    <Tab title="导航 22222222">内容 222222</Tab>
+  </Tabs>
+</template>
+<script lang="ts" setup>
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import { ref } from 'vue';
+
+const xxx = ref('导航 22222222')
+
+</script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,28 +1,9 @@
 <template>
   <div>Tabs 示例</div>
-  <h1>示例 1</h1>
-  <Tabs v-model:selected="xxx">
-    <Tab title="导航 1">内容 1</Tab>
-    <Tab title="导航 22222222">内容 222222</Tab>
-  </Tabs>
+  <Demo :component="Tabs1Demo"></Demo>
 </template>
 
-<script lang="ts">
-import Tabs from '../lib/Tabs.vue'
-import Tab from '../lib/Tab.vue'
-import { ref } from 'vue';
-
-export default {
-  components: {
-    Tabs,
-    Tab,
-  },
-  setup() {
-    const xxx = ref('导航 22222222')
-
-    return {
-      xxx
-    }
-  }
-}
+<script lang="ts" setup>
+import Demo from './Demo.vue';
+import Tabs1Demo from './Tabs1.demo.vue';
 </script>
