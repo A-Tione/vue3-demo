807fdfcde60cb4055a94fadc4a8a457732fd31f4
Yang.he <yang.he@homepartners.tech>
2024-10-25 14:15:52 +0800

    feat: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git add . &&
 git commit -m 'update' &&
 git branch -M main &&
 git remote add origin https://github.com/A-Tione/vue3-atee-ui-demo.git
-git push -u origin main
+git push -f origin main
 cd -
 echo https://github.com/A-Tione/vue3-atee-ui-demo/index.html
