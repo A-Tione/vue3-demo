309fd365bcd5514ea3568abd9718de8f93c09803
A-Tione <heyanggo@gmail.com>
2024-12-16 11:16:03 +0800

    chore: modify reops name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { baseParse, ElementNode } from '@vue/compiler-core';
 
 export default {
   plugins: [md()],
-  base: '/vue3-atee-ui-demo/',
+  base: '/vue3-demo/',
   assetsDir: 'assets', // 将默认的 _assets 改为 _custom_assets
   vueCustomBlockTransforms: {
     demo: (options) => {
